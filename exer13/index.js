bd1b4fb307342ab4f05faba9cfc98c7f14b9225a
Lucas Santos <lucas22.ferreira.007@gmail.com>
2023-07-28 20:41:55 -0300

    feito o exer13

--- exer13/index.js
+++ exer13/index.js
@@ -1,4 +1,8 @@
-
+const num = parseInt(prompt('Digite um número: '))
+  if(num < 1 || num == '' || num == isNaN(num)) document.write('DIGITE UM VALOR VÁLIDO!')
+for(let i = 1; i <= num; i+=2){
+  document.write(`${i} `)
+}
 
 // 13. Faça um algoritmo que armazene um número e imprima os
 // números ímpares entre 1 e o número armazenado.
