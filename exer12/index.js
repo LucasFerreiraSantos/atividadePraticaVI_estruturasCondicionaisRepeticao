111bc6937f7b675db988e0987375527a3d6a4a9a
Lucas Santos <lucas22.ferreira.007@gmail.com>
2023-07-28 20:35:12 -0300

    feito o exer12

--- exer12/index.js
+++ exer12/index.js
@@ -1,4 +1,9 @@
-
+const idade = Number(prompt('Qual a sua idade: '))
+  if(idade >=18 && idade <= 67) {
+    document.write(`VOCÊ pode doar sangue!`)
+  } else{
+    document.write(`VOCÊ NÃO PODE DOAR SANGUE!`)
+  }
 
 // 12. Para doar sangue é necessário ter entre 18 e 67 anos. Faça um
 // programa que armazene a idade de uma pessoa e diga se ela pode
