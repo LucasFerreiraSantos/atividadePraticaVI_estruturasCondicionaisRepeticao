b83bd1e7fff78641fb2f40d0332982611286528b
Lucas Santos <lucas22.ferreira.007@gmail.com>
2023-07-28 20:22:35 -0300

    feito o exer11

--- exer10/index.js
+++ exer10/index.js
@@ -1,5 +1,17 @@
+const altura = parseFloat(prompt('Digita a sua altura: '))
+const sexo = prompt('Qual o seu sexo:\n1.Feminino;\n2.Masculino.')
 
-
+  switch(+sexo){
+    case 1:
+      const pesoIdealFeminino = (62.1 * altura) - 44.7
+      document.write(`Seu peso ideal é ${pesoIdealFeminino.toFixed(2)}`)
+      break
+    case 2:
+      const pesoIdealMasculino = (72.7 * altura) - 58
+      document.write(`Seu peso ideal é ${pesoIdealMasculino.toFixed(2)}`)
+      break
+  }
+  
 // 10. Tendo como entrada a altura e o sexo (codificado da seguinte
 //   forma: 1 para sexo feminino e 2 para sexo masculino) de uma
 //   pessoa, construa um programa que calcule e mostre seu peso ideal,
--- exer11/index.js
+++ exer11/index.js
@@ -1,4 +1,18 @@
+const turno = prompt('Qual o seu turno:\nM.Matutino;\nV.Vespertino;\nN.Noturno.').toUpperCase()
 
+  switch(turno){
+    case 'M':
+      document.write('BOM DIA!!!')
+      break
+    case 'V':
+      document.write('BOA TARDE!!!')
+      break
+    case 'N':
+      document.write('BOA NOITE!!!')
+      break
+    default:
+      document.write('VALOR INVÁLIDO!')
+  }
 
 // 11. Utilizando Switch, faça um programa que pergunte em que turno
 // você estuda. Peça para digitar “M” para matutino ou “V” para
