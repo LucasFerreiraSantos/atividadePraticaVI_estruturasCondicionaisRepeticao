5a4c9205876d57ee40282c55f6862bf6afd6e4fb
Lucas Santos <lucas22.ferreira.007@gmail.com>
2023-07-27 22:21:40 -0300

    feito o exer2

--- exer2/index.js
+++ exer2/index.js
@@ -1,4 +1,15 @@
-
+const idadeMotorista = Number(prompt('Qual a sua idade? '))
+const possuiHabilitacao = confirm('Você possui habilitação?')
+  if(idadeMotorista < 18){
+    document.write(`Sua idade é ${idadeMotorista} anos, você NÃO pode dirigir!!!`)
+  } else{
+    document.write(`Sua idade é ${idadeMotorista} anos, você é maior de idade! `)
+      if(possuiHabilitacao === true){
+        document.write('Você PODE DIRIGIR!')
+      } else{
+        document.write('Você NÃO PODE DIRIGIR!')
+      }
+  }
 
 // 2. Para o mesmo exercício acima insira mais uma variável a
 // condicional, além de saber se o motorista tem 18 anos ou mais,
