0427fbf7dc95a852c2b48b41ce9031eff03944c9
Lucas Santos <lucas22.ferreira.007@gmail.com>
2023-07-28 18:43:40 -0300

    feito o exer7

--- exer7/index.js
+++ exer7/index.js
@@ -1,4 +1,8 @@
-
+let contador = 10
+  do{
+    document.write(`${contador} `)
+    contador++
+  }while(contador <= 100)
 
 // 7. Utilizando do...while, imprima na tela a soma de todos os números
 // entre 10 e 100.
