4b1fa4b5043d605b0034d135e1b150f001a8291a
Lucas Santos <lucas22.ferreira.007@gmail.com>
2023-07-28 18:50:54 -0300

    feito o exer8

--- exer8/index.js
+++ exer8/index.js
@@ -1,4 +1,10 @@
-
+const salario = parseFloat(prompt('Diga o seu salário: '));
+const limiteIR = 1903.98;
+  if(salario <= limiteIR){
+    document.write(`Seu salário é de R$${salario.toFixed(2)}. Você está isento do pagamento do Imposto de Renda.`)
+  } else{
+    document.write(`Seu salário é de R$${salario.toFixed(2)}. Você será tributado ao pagamento do Imposto de Renda.`)
+  }
 
 // 8. Crie uma variável para armazenar o salário do usuário e atribua um
 // valor a essa variável. Crie a validação necessária:
