59d0e6817202080aa0c734fbf9b66aa2d3007aef
Lucas Santos <lucas22.ferreira.007@gmail.com>
2023-07-28 19:41:41 -0300

    feito o exer9

--- exer9/index.js
+++ exer9/index.js
@@ -1,4 +1,13 @@
+let num = Number(prompt('Digite um número, para verificar se ele é primo: '))
+let numPrimo = true
 
+  let divisores=0;
+
+  for(let i=1; i<=num; i++)
+    if(num % i === 0) divisores++;
+
+    if(divisores==2) document.write(`Você digitou ${num}, ele é primo.`);
+    else document.write(`Você digitou ${num}, ele não é primo`);
 
 // 9. Informe um valor a uma variável e imprima no console se o número
 // é primo.
