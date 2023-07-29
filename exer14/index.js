de9ba87c6e7aba3c05a22c0ab8515fa85161a48e
Lucas Santos <lucas22.ferreira.007@gmail.com>
2023-07-28 21:58:28 -0300

    feito o exer14

--- exer14/index.js
+++ exer14/index.js
@@ -1,4 +1,15 @@
-
+let num = Number(prompt('Digite um número: '))
+let resultado = 0
+  document.write(`${num} `)
+  while(resultado < 500){
+    const multiplicador = 3
+    let soma = num * multiplicador
+    num = soma
+    resultado += soma
+      if(soma <= 500){
+        document.write(`${soma} `)
+      }
+  }
 
 // 14. Leia um número, utilizando WHILE multiplique este número por 3
 // enquanto a soma seja menor que 500 e no final mostre qual o
