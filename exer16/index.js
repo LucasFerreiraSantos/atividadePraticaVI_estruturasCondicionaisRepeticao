dc01c720bd0f47fcaf2fcafa9db4ccacad16f2dc
Lucas Santos <lucas22.ferreira.007@gmail.com>
2023-07-30 19:42:42 -0300

    feito o exer16 de verificação de voto

--- exer16/index.js
+++ exer16/index.js
@@ -1,4 +1,14 @@
-
+const dataAtual = new Date();
+const anoAtual = dataAtual.getFullYear();
+const anoNascimento = Number(prompt('Qual o seu ano de nascimento? '))
+const calculoIdade = anoAtual - anoNascimento
+  if(calculoIdade < 16){
+    document.write(`Sua idade é ${calculoIdade} anos. Você NÃO PODE VOTAR!`)
+  } else if(calculoIdade >= 16 && calculoIdade < 18 || calculoIdade > 65){
+    document.write(`Sua idade é ${calculoIdade} anos. Seu voto é OPCIONAL!`)
+  } else{
+    document.write(`Sua idade é ${calculoIdade} anos. Você PODERÁ VOTAR!`)
+  }
 
 // 16. Escreva um algoritmo que armazene o ano atual e o ano de
 // nascimento de uma pessoa. Escrever uma mensagem no console
